d7d932286abbfcf405dde3b6e07a20984b026e4f
timmedvedevaudio <timmedvedevaudio@gmail.com>
2025-12-17 21:00:13 +0100

    Update timm_move note selection left.lua

--- scripts/timm_move note selection left.lua
+++ scripts/timm_move note selection left.lua
@@ -3,60 +3,52 @@
 -- Description: Deselects current notes and selects the previous single note to the left.
 
 function SelectPreviousNote()
-    -- Get the active MIDI editor
     local midi_editor = reaper.MIDIEditor_GetActive()
     if not midi_editor then return end
 
-    -- Get the current take being edited
     local take = reaper.MIDIEditor_GetTake(midi_editor)
     if not take then return end
 
-    -- Count total notes in the take
-    local _, note_count = reaper.MIDI_CountEvts(take)
+    local note_count = reaper.MIDI_CountEvts(take)
     if note_count == 0 then return end
 
-    -- 1. Find the "Left-Most" selected note
-    -- We need to find the selected note with the lowest index
     local first_selected_index = -1
     local any_selected = false
 
+    -- Find first selected note
     for i = 0, note_count - 1 do
-        local _, selected, _, _, _, _, _, _ = reaper.MIDI_GetNote(take, i)
+        local _, selected = reaper.MIDI_GetNote(take, i)
         if selected then
             first_selected_index = i
             any_selected = true
-            break -- We found the first one, no need to check the rest
+            break
         end
     end
 
-    -- 2. Determine which note to select next
     local target_note_index = -1
 
     if not any_selected then
-        -- If nothing is selected, select the very last note (logical reverse of the other script)
         target_note_index = note_count - 1
     else
-        -- If something is selected, select the previous note (index - 1)
         if first_selected_index > 0 then
             target_note_index = first_selected_index - 1
         else
-            -- If the first note (index 0) is already selected, do nothing
-            return 
+            return -- Already at the first note
         end
     end
 
-    -- 3. Apply the Selection
     reaper.MIDI_DisableSort(take)
+    reaper.MIDIEditor_OnCommand(midi_editor, 40214) -- Deselect all
 
-    -- Deselect all notes (Command ID 40214)
-    reaper.MIDIEditor_OnCommand(midi_editor, 40214)
-
-    -- Select the target note
     local retval, selected, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, target_note_index)
-    reaper.MIDI_SetNote(take, target_note_index, true, muted, startppq, endppq, chan, pitch, vel, true)
 
-    -- Optional: Move cursor to the start of the new note
-reaper.SetEditCurPos(reaper.MIDI_GetProjTimeFromPPQ(take, startppq), true, false)
+    if retval then
+        reaper.MIDI_SetNote(take, target_note_index, true, muted, startppq, endppq, chan, pitch, vel, true)
+        
+        -- FIX: Use Native Action to move cursor to the selected note
+        -- 40440 = Navigate: Move edit cursor to start of selected events
+        reaper.MIDIEditor_OnCommand(midi_editor, 40440)
+    end
 
     reaper.MIDI_Sort(take)
 end
